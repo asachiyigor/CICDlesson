8ac493a1d72b151ced3b97578ded0beddc2b8547
asachiyigor <asachiyigor@gmail.com>
2022-03-31 15:38:52 +0300

    test success

---
+++ src/test/java/MainTest.java
@@ -0,0 +1,15 @@
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void findMax() {
+        Main service = new Main();
+        long[] incomesInBillions = {12, 5, 8, 4, 5, 3, 8, 6, 11, 11, 12};
+        long expected = 12;
+        long actual = service.findMax(incomesInBillions);
+        assertEquals(expected, actual);
+    }
+}
